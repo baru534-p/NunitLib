7bf980ac8cd6ecb306b374b4ae1379e4808944c1
TavvaBhargavi <baru.tavva@gmail.com>
2023-12-07 19:09:17 +0530

    Add project files.

---
+++ TestProject1/StudentTest.cs
@@ -0,0 +1,35 @@
+﻿using NunitLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+   public class StudentTest
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "bhargavi";
+            // Act
+            string studentName = student.Name;
+            // Assert
+            Assert.AreEqual("bhargavi", studentName);
+        }
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "123 Main St";
+            // Act
+            string studentAddress = student.Address;
+            // Assert
+            Assert.AreEqual("123 Main St", studentAddress);
+        }
+    }
+}
